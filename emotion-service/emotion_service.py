7f66f8a79518c086b5957a4384efa49f65347d3f
Kamil Magryta <gryxon@gmail.com>
2017-05-21 12:06:19 +0200

    Add Cors.

--- emotion-service/emotion_service.py
+++ emotion-service/emotion_service.py
@@ -10,8 +10,11 @@ from pymongo import MongoClient
 from bson.objectid import ObjectId
 import copy
 import time
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+CORS(app)
 mongo_client = MongoClient()["emotion_faces"]["emotion_entities"]
 
 
